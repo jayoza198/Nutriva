93dd96f1f1b1eefc01cbb8378338402a027a02fd
Jay Oza <71382456+jayoza198@users.noreply.github.com>
2023-06-08 14:28:59 +0530

    Add files via upload

--- app1.py
+++ app1.py
@@ -114,46 +114,121 @@ def display_top_foreign_companies_page():
 #         st.markdown("Importer not found.")
 
 # Function to search for an exporter
-def search_exporter_page():
+# def search_exporter_page():
+#     st.title("Search Exporter")
+#     exporter_name = st.text_input("Enter the exporter name:")
+    
+#     if exporter_name:
+#         exporter_data = df[df['IndianCompany'].str.contains(exporter_name, case=False)]
+    
+#         if len(exporter_data) > 0:
+#             st.markdown(f"### Exporter: {exporter_name}")
+#             st.dataframe(exporter_data)
+#         else:
+#             st.markdown("Exporter not found.")
+
+def search_exporter_page(df):
     st.title("Search Exporter")
-    exporter_name = st.text_input("Enter the exporter name:")
-    
+    exporter_name = st.text_input("Enter the Indian exporter:")
+
     if exporter_name:
-        exporter_data = df[df['IndianCompany'].str.contains(exporter_name, case=False)]
-    
-        if len(exporter_data) > 0:
-            st.markdown(f"### Exporter: {exporter_name}")
-            st.dataframe(exporter_data)
+        # Check if the input exporter is in the DataFrame
+        if exporter_name in df['IndianCompany'].unique():
+            # Filter the DataFrame for the selected exporter
+            filtered_df = df[df['IndianCompany'] == exporter_name]
+
+            # Group the data by Foreign Company and Foreign Country
+            grouped_data = filtered_df.groupby(['ForeignCompany', 'ForeignCountry']).agg({'FOB INR': 'sum'}).reset_index()
+
+            # Sort the data by FOB INR column in descending order
+            grouped_data = grouped_data.sort_values('FOB INR', ascending=False)
+
+            # Calculate and display the total FOB INR for all transactions
+            total_fob_inr = grouped_data['FOB INR'].sum()
+
+            # Display the results in a tabular format
+            table_data = grouped_data.values.tolist()
+            headers = ['Foreign Company', 'Foreign Country', 'FOB INR']
+
+            # Format FOB INR values with commas and 2 decimal places
+            formatted_table_data = [[company, country, f"{inr:,.2f}"] for company, country, inr in table_data]
+
+            st.markdown("### Export Details")
+            st.markdown(f"#### Indian Exporter: {exporter_name}")
+            st.markdown(tabulate(formatted_table_data, headers=headers, tablefmt='pipe'))
+
+            # Display the total FOB INR for all transactions without scientific notation
+            st.markdown(f"Total FOB INR for all transactions: {format(total_fob_inr, ',')}")
         else:
-            st.markdown("Exporter not found.")
+            st.markdown("Invalid Indian exporter. Please enter a valid exporter.")
+    
 
 # Function to search for an importer
-def search_importer_page():
+def search_importer(df, foreign_company):
     st.title("Search Importer")
-    importer_name = st.text_input("Enter the importer name:")
-    
-    if importer_name:
-        importer_data = df[df['ForeignCompany'].str.contains(importer_name, case=False)]
-    
-        if len(importer_data) > 0:
-            st.markdown(f"### Importer: {importer_name}")
-            st.dataframe(importer_data)
+    foreign_company_name = st.text_input("Enter the foreign company:")
+
+    if foreign_company_name:
+        # Check if the input importer is in the DataFrame
+        if foreign_company_name in df['ForeignCompany'].unique():
+            # Filter the DataFrame for the selected importer
+            filtered_df = df[df['ForeignCompany'] == foreign_company_name]
+
+            # Get the number of Indian companies associated with the foreign company
+            num_indian_companies = len(filtered_df['IndianCompany'].unique())
+
+            # Get the list of Indian companies and corresponding FOB INR values associated with the foreign company
+            indian_companies = filtered_df['IndianCompany'].unique().tolist()
+            fob_inr_values = filtered_df.groupby('IndianCompany')['FOB INR'].sum().tolist()
+
+            # Create a DataFrame for Indian companies and FOB INR
+            table_data = {'IndianCompany': indian_companies, 'FOB INR': fob_inr_values}
+            df_importer = pd.DataFrame(table_data)
+
+            # Sort the DataFrame by FOB INR column in descending order
+            df_importer = df_importer.sort_values('FOB INR', ascending=False)
+
+            # Format FOB INR values with commas and 2 decimal places
+            df_importer['FOB INR'] = df_importer['FOB INR'].apply(lambda x: f"{x:,.2f}")
+
+            st.markdown("### Import Details")
+            st.markdown(f"#### Foreign Importer: {foreign_company_name}")
+            st.markdown(f"Number of Indian Companies associated with {foreign_company_name}: {num_indian_companies}")
+            st.markdown("List of Indian Companies and FOB INR:")
+            st.markdown(df_importer.to_markdown(index=False))
         else:
-            st.markdown("Importer not found.")
+            st.markdown("Invalid foreign importer. Please enter a valid importer.")
+    
+    
+# Function to search for an importer
+# def search_importer_page():
+#     st.title("Search Importer")
+#     importer_name = st.text_input("Enter the importer name:")
+    
+#     if importer_name:
+#         importer_data = df[df['ForeignCompany'].str.contains(importer_name, case=False)]
+    
+#         if len(importer_data) > 0:
+#             st.markdown(f"### Importer: {importer_name}")
+#             st.dataframe(importer_data)
+#         else:
+#             st.markdown("Importer not found.")
+
 
 def main():
     st.title("Nutriva Lifesciences")
     st.markdown("### Welcome to the Nutriva Lifesciences Analysis App!")
 
     pages = {
-        "Top Exporters": top_exporters_page,
-        "Top Importers": top_importers_page,
-        "Top Products": top_products_page,
-        "Top Products by Country": display_top_products_by_country_page,
-        "Top Foreign Companies": display_top_foreign_companies_page,
-        "Search Exporter": search_exporter_page,
-        "Search Importer": search_importer_page
-    }
+    "Top Exporters": top_exporters_page,
+    "Top Importers": top_importers_page,
+    "Top Products": top_products_page,
+    "Top Products by Country": display_top_products_by_country_page,
+    "Top Foreign Companies": display_top_foreign_companies_page,
+    "Search Exporter": lambda: search_exporter_page(df),
+    "Search Importer": lambda: search_importer_page(df)
+}
+
 
     # Add a sidebar to the app
     st.sidebar.title("Navigation")
